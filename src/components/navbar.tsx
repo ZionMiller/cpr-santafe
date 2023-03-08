44611e60c0291cf2f28ad454e59a1cfab6b8a5cb
Zion Miller <djzionmiller@gmail.com>
2023-03-08 12:44:46 -0800

    fixed nav route

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -68,7 +68,7 @@ const Navbar = () => {
               <a className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">ABOUT</a>
             </li>
           </Link>
-          <Link href="/schedLanding">
+          <Link href="/useLanding">
             <li>
               <a className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">CLASSES</a>
             </li>
